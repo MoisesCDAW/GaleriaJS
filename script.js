35c1ae0ec55729c599c85ce5d6092b8ada550c02
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-16 23:02:47 +0000

    Se pintan las fotos obtenidas de la API con las frases en sus respectivas posiciones y se hizo un avance en la muestra de recursos en la sección "Videos"

--- script.js
+++ script.js
@@ -1,11 +1,77 @@
 // Filtros Imágenes: Por orientación (landscape, portrait o square) o color (ej: red, orange, etc...)
-// Filtros Videos: Por orientación (landscape, portrait o square) o por tamaño (ej: large, medium o small)
+// Filtros Videos: Por calidad (ej: sd, hd o uhd)
 
-const APIDefecto = "https://api.pexels.com/v1/search?query=sky";
-// const API = "https://api.pexels.com/videos/search?query=nature";
-let datosAPI;
+/**
+ * Frases por defecto que cargan siempre en la misma posición en la galería.
+ * Estas frases acompañan a las imágenes mostradas
+ */
+const frasesFotos = [
+    {
+        frase: "Fotos para cada necesidad, al alcance de un clic y totalmente gratuitas.",
+        posicion: "1 / 1 / 2 / 2"
+    },
+    {
+        frase: "Descubre el poder de una buena imagen para llevar tus proyectos al siguiente nivel.",
+        posicion: "2 / 3 / 3 / 4"
+    },
+    {
+        frase: "Cada imagen es una oportunidad para contar una historia, elige la tuya.",
+        posicion: "4 / 3 / 5 / 4"
+    },
+    {
+        frase: "De lo clásico a lo moderno: encuentra la foto perfecta para tu visión.",
+        posicion: "5 / 1 / 6 / 2"
+    }
+];
+
+/**
+ * Frases por defecto que cargan siempre en la misma posición en la galería.
+ * Estas frases acompañan a los videos mostrados
+ */
+const frasesVideos = [
+    {
+        frase: "Fotos para cada necesidad, al alcance de un clic y totalmente gratuitas.",
+        posicion: "1 / 1 / 2 / 2"
+    },
+    {
+        frase: "Descubre el poder de una buena imagen para llevar tus proyectos al siguiente nivel.",
+        posicion: "2 / 3 / 3 / 4"
+    },
+    {
+        frase: "Cada imagen es una oportunidad para contar una historia, elige la tuya.",
+        posicion: "4 / 3 / 5 / 4"
+    },
+    {
+        frase: "De lo clásico a lo moderno: encuentra la foto perfecta para tu visión.",
+        posicion: "5 / 1 / 6 / 2"
+    }
+];
+
+/**
+ * API para mostrar imágenes por defecto al cargar la página en la sección "Fotos"
+ */
+const APIDefectoFotos = "https://api.pexels.com/v1/search?query=sky";
+
+/**
+ * API para mostrar videos por defecto al cargar la página en la sección "Videos"
+ */
+const APIDefectoVideos = "https://api.pexels.com/videos/search?query=nature";
 
 
+/**
+ * Crea un nuevo elemento HTML con los atributos y clases proporcionadas.
+ *
+ * @function crearElemento
+ * @param {string} tipo - El tipo de elemento HTML a crear (por ejemplo, 'div', 'img', 'a', etc.).
+ * @param {Object} [atributos={}] - Un objeto que contiene los atributos a asignar al elemento. Las claves son los nombres de los atributos y los valores son los valores que se asignarán.
+ * @param {Array<string>} [clases=[]] - Un array de cadenas con las clases CSS a agregar al elemento.
+ * @returns {HTMLElement} El elemento HTML creado con los atributos y clases especificadas.
+ *
+ * @example
+ * // Crear un <div> con una clase y un atributo
+ * const div = crearElemento('div', {id: 'miDiv', title: 'Este es un div'}, ['clase1', 'clase2']);
+ * document.body.append(div);
+ */
 function crearElemento(tipo, atributos={}, clases=[]){
     let elemt = document.createElement(tipo);
 
@@ -23,10 +89,40 @@ function crearElemento(tipo, atributos={}, clases=[]){
 }
 
 
-function constructor_galeria(datosAPI){
+function addFrases(frases) {
+    frases.forEach((frase)=>{
+        let content = crearElemento("div", {style:`grid-area: ${frase.posicion};`}, ["frases"]);
+        let text = crearElemento("p", {style:"font-size: 16px;"});
+        document.querySelector(".galeria").append(content);
+        content.append(text);
+        text.append(frase.frase);
+    });
+}
+
+
+function crearGaleriaVideos(datosAPI){
+    let videos = datosAPI.videos;
+    let galeria = document.querySelector(".galeria");
+
+    // Construye los contenedores para cada foto y asigna la foto
+    videos.forEach((video)=>{
+        let content = crearElemento("div");
+        let vid = crearElemento("video", {src:video.video_files[1].link, controls:"controls"});
+
+        galeria.append(content);
+        content.append(vid);
+    });
+
+    // Agrega los contenedores para cada foto-frase y asigna la frase
+    addFrases(frasesVideos);
+}
+
+
+function crearGaleriaFotos(datosAPI){
     let imagenes = datosAPI.photos;
     let galeria = document.querySelector(".galeria");
 
+    // Construye los contenedores para cada foto y asigna la foto
     imagenes.forEach((imagen)=>{
         let content = crearElemento("div");
         let img = crearElemento("img", {src:imagen.src.portrait});
@@ -34,50 +130,57 @@ function constructor_galeria(datosAPI){
         galeria.append(content);
         content.append(img);
     });
+
+    // Agrega los contenedores para cada foto-frase y asigna la frase
+    addFrases(frasesFotos);
 }
 
 
 /**
- * Permite consultar recursos de una API. La consulta se gestiona con Fetch
- * @param {String} API que se consultará
+ * Obtiene datos de una API a través de una solicitud HTTP GET utilizando Axios.
+ * 
+ * @async
+ * @function obtenerDatos
+ * @param {string} API - La URL de la API desde donde se obtendrán los datos.
+ * @returns {Promise<Object|undefined>} Una promesa que se resuelve con los datos obtenidos de la API (como un objeto).
+ *                                       Retorna `undefined` si ocurre un error durante la solicitud.
+ * @throws {Error} Registra el error en la consola si la solicitud falla.
  */
 async function obtenerDatos(API) {
-    fetch(API, {
-        headers: {Authorization: "bt7O8cBT2GxgAPoNGeOjEHukUu3IxrVJiBTiMQg0iNTnomw6qAx6VCM9"}
-    })
-
-        // Comprobamos que no hay errores al consultar la API
-        .then(respuesta => {
-            if (respuesta.ok) {
-                return respuesta.json();
-            }else {
-                // Creamos un error y automáticamente se ejecuta el catch
-                throw new Error(respuesta.status);
+    try {
+        const datosAPI = await axios.get(API, {
+            headers: {
+                Authorization: 'bt7O8cBT2GxgAPoNGeOjEHukUu3IxrVJiBTiMQg0iNTnomw6qAx6VCM9',
             }
-        })
-
-        // Tomamos los datos y los guardamos en una variable global "Datos"
-        .then(datos => {
-            datosAPI = datos;
-            constructor_galeria(datosAPI);
-        })
-
-        .catch(err => {
-            console.error("ERROR: ", err.message)
         });
+        return datosAPI.data;
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+    }
 }
 
 
 /**
- * Permite marcar en qué página se está actualmente.
+ * Cambia la visibilidad de las secciones de fotos y videos en función del contenido del título de la página.
+ * Si el título es "Fotos", muestra la galería de fotos; si no, muestra la galería de videos.
+ * 
+ * Llama a la función `obtenerDatos` para obtener los datos de la API correspondiente (fotos o videos) y luego pasa esos datos a las funciones `constructor_galeriaFotos` o `constructor_galeriaVideos`.
+ *
+ * @function ubicacion
+ * @throws {Error} Lanza un error si no encuentra los elementos con los selectores `#fotos`, `#videos` o `title` en el DOM.
+ * 
+ * @global {string} APIDefectoFotos - URL de la API para obtener las fotos.
+ * @global {string} APIDefectoVideos - URL de la API para obtener los videos.
  */
 function ubicacion() {
     if (document.querySelector("title").textContent=="Fotos") {
         document.querySelector("#videos").classList.toggle("pag-no-actual");
+        obtenerDatos(APIDefectoFotos).then(datosAPI => crearGaleriaFotos(datosAPI));
+
     }else {
         document.querySelector("#fotos").classList.toggle("pag-no-actual");
+        obtenerDatos(APIDefectoVideos).then(datosAPI => crearGaleriaVideos(datosAPI));
     }
 }
 
-// obtenerDatos(APIDefecto);
 ubicacion();
